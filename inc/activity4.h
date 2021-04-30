f4a6a8696a369b929ce643fe4412e2e90635afc5
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-04-30 18:25:43 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,18 +1,34 @@
+/**
+ * @file activity4.h
+ * @author Annappa (annappa211@gmail.com)
+ * @brief activity4-header file
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY4_H_INCLUDED
 #define ACTIVITY4_H_INCLUDED
-/** \brief initialize USART
- *
- * \param ubrr_value
- *
- */
 
+/**
+ * @brief Function to initialize USART
+ * 
+ * @param ubrr_value 
+ */
 void USARTInit(uint16_t ubrr_value);
-/** \brief send the the modified temperature value to USART 
- *
- * \param temp_value
- */
 
+/**
+ * @brief Function to send the the modified temperature value to USART
+ * 
+ * @param temp_value 
+ */
 void USARTWrite(char temp_value);
 
+/**
+ * @brief Function to display the temperature value on the serial monitor screen
+ * 
+ * @return char 
+ */
 char temp_disp();
 #endif // ACTIVITY4_H_INCLUDED
