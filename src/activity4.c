820853f1b8c0ce1b1c2bf49cc78351db3bc33f45
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-04-30 18:38:48 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -1,8 +1,23 @@
+/**
+ * @file activity4.c
+ * @author Annappa M (annappa211@gmail.com)
+ * @brief Dsiplay the value of temperature on the serial monitor
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #include "activity2.h"
 #include "activity3.h"
 #include "activity4.h"
 
+/**
+ * @brief Function to initialize USART
+ * 
+ * @param ubrr_value 
+ */
 void USARTInit(uint16_t ubrr_value)
 {
     UBRR0L = ubrr_value;
@@ -11,6 +26,12 @@ void USARTInit(uint16_t ubrr_value)
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
 }
+
+/**
+ * @brief Function to send the the modified temperature value to USART
+ * 
+ * @param temp_value 
+ */
 void USARTWrite(char temp_value)
 {
   while(!(UCSR0A & (1<<UDRE0)))
@@ -18,6 +39,11 @@ void USARTWrite(char temp_value)
 UDR0 = temp_value;
 }
 
+/**
+ * @brief Function to display the temperature value on the serial monitor screen
+ * 
+ * @return char 
+ */
 char temp_disp()
 {
     char temperature;
