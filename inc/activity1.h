6ca89aeb41d5f593f91f9fb3fc8d6c3cbb2dceca
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-04-30 17:35:30 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,9 +1,26 @@
+/**
+ * @file activity1.h
+ * @author Annappa (annappa211@gmail.com)
+ * @brief activity1-header file
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY1_H_INCLUDED
 #define ACTIVITY1_H_INCLUDED
 
-void initialise();//TO INITIALISE PORTS
+/**
+ * @brief FUNCTION TO INITIALISE PORTS
+ * 
+ */
+void initialise();
 
-#define occupied (!(PIND&(3<<PD0)))//Check if seat is occupied
-
+/**
+ * @brief A macro to Check if seat is occupied
+ * 
+ */
+#define occupied (!(PIND&(3<<PD0)))
 
 #endif
