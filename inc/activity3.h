da82beb6d3b8044335f334484fa2eb474935af09
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-04-30 18:21:46 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,7 +1,26 @@
+/**
+ * @file activity3.h
+ * @author Annappa (annappa211@gmail.com)
+ * @brief activity3-header file
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
 
+/**
+ * @brief Function to initialize timer registers
+ * 
+ */
 void config_timer(void);
+
+/**
+ * @brief Function to produce duty cycle in PWM according to input ADC value
+ * 
+ */
 void pwm_output(void);
 
 #endif
